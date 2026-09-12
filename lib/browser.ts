83bef9c419b9d628d2a9833f2afa7f351cff7e4e
Severin Ibarluzea <seveibar@gmail.com>
2026-09-12 14:34:09 -0700

    Build and publish a standalone browser module for CDN imports (#66)

---
+++ lib/browser.ts
@@ -0,0 +1,9 @@
+// This entrypoint is bundled with all dependencies for direct browser imports.
+export {
+  analyzeSchematicPlacement,
+  SchematicPlacementAnalysis,
+} from "./analyze-schematic-placement"
+export {
+  createSchematicPlacementIssueArtifacts,
+  type SchematicPlacementIssueArtifact,
+} from "./create-schematic-placement-issue-artifacts"
